769b6ef36a5f850a3f90d5eb5a29a8858daf0f66
liuduan <liuduan.05.05@163.com>
2018-04-04 15:20:33 +0800

    modify image

--- src/pages/index/containers/index/index.js
+++ src/pages/index/containers/index/index.js
@@ -3,14 +3,12 @@ import { Link } from 'react-router'
 import './index.scss'
 import Icon from './img/icon.png'
 import MiddleImg from './img/react生命周期.jpg'
-import BigImg from './img/微众前端交互.png'
 
 const Main = props => (
     <div className="hello">
         <Link to="/list">mains 1sss</Link>
         <img src={Icon} alt=""/>
         <img src={MiddleImg} height="200" width="50" alt=""/>
-        <img src={BigImg} height="200" width="200" alt=""/>
     </div>
 )
 
